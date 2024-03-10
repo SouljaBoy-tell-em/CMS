702c6cf0b0f5d34e551ede50b900f40a9ce957be
SouljaBoy-tell-em <60592559+SouljaBoy-tell-em@users.noreply.github.com>
2024-03-10 16:18:48 +0300

    Update L-stability.py

--- Hard HDE/L-stability.py
+++ Hard HDE/L-stability.py
@@ -4,12 +4,11 @@ import matplotlib.pyplot as plt
 
 T = 3
 N = 31
-h = T/(N-1)
-x_teor = np.linspace(0,T,(N-1)*100+1)
-x = np.linspace(0,T,N)
+h = T/(N - 1)
+x_teor = np.linspace(0, T, (N - 1) * 100 + 1)
+x = np.linspace(0, T, N)
 l = -1e6
 u = np.zeros(N)
-
 
 #implicit  Euler method
 def im_euler(u, l, h, N):
@@ -19,28 +18,30 @@ def im_euler(u, l, h, N):
 #trapezoidal method
 def trapezoidal(u, l, h, N):
     for i in range(0, N - 1):
-        free = l * (np.cos((i + 1) * h) + np.cos(i * h) + (np.sin((i + 1) * h) + np.sin(i * h)))
-        u[i + 1] = (u[i] * (1 + l * h / 2) - h / 2 * free) / (1 - l * h / 2)
+        free = l * (np.cos((i + 1) * h) + np.cos(i * h)) + (np.sin((i + 1) * h) + np.sin(i * h))
+        u[i + 1] = (u[i] * (1 + l * h / 2) - h / 2 * free)/(1 - l * h / 2)
 
 
 
 figure, axis = plt.subplots(2, 2)
 
-im_euler(u, l, h, N)
 u[0] = 1
+im_euler(u, l, h, N)
 axis[0, 0].plot(x_teor, np.cos(x_teor), color='g', label='exact')
 axis[0, 0].plot(x, u, 'ro', label='implicit Euler')
 u[0] = 1.5
+im_euler(u, l, h, N)
 axis[0, 1].plot(x_teor, np.cos(x_teor), color='g', label='exact')
 axis[0, 1].plot(x, u, 'ro', label='implicit Euler')
 
 
 
-trapezoidal(u, l, h, N)
 u[0] = 1
+trapezoidal(u, l, h, N)
 axis[1, 0].plot(x_teor, np.cos(x_teor), color='g', label='exact')
 axis[1, 0].plot(x, u, 'ro', label='trapezoidal')
 u[0] = 1.5
+trapezoidal(u, l, h, N)
 axis[1, 1].plot(x_teor, np.cos(x_teor), color='g', label='exact')
 axis[1, 1].plot(x, u, 'ro', label='trapezoidal')
 
